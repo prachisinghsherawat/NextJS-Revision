11073df79d7856b5cfe76c6d9dba3df8fe3f876e
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2023-01-24 22:39:34 +0530

    nextJS revision

---
+++ fetch/pages/products/index.tsx
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router"
+
+
+export default function Product(){
+
+    const router = useRouter()
+
+    return(
+        <>
+        <h1>Products</h1>
+
+        <button onClick={() => router.push('/products/id')}>Click Here</button>
+        </>
+    )
+}
